0881e2b51f88ac054fe846f4b4122ebcfc204b47
muddassirk <mkkhanu72@gmail.com>
2021-11-16 11:25:39 +0500

    add artical comp

--- src/components/artical/Artical.jsx
+++ src/components/artical/Artical.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import './artical.css'
 
-const Artical = () => {
-    return (
+const Artical = ({ imgUrl, date, text }) => (
+    <div className="gpt3__blog-container_article">
+      <div className="gpt3__blog-container_article-image">
+        <img src={imgUrl} alt="blog_image" />
+      </div>
+      <div className="gpt3__blog-container_article-content">
         <div>
-            Artical
+          <p>{date}</p>
+          <h3>{text}</h3>
         </div>
-    )
-}
-
+        <p>Read Full Article</p>
+      </div>
+    </div>
+  );
+  // 2:49:41
 export default Artical
